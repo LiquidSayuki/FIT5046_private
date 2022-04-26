d3d75a1cc005509ce31e6d6175a1586c2bb97a82
LiquidSayuki <liquidsayuki@icloud.com>
2022-04-26 16:54:16 +1000

    week8. Recycler View！

---
+++ tutorial8/src/main/java/com/liquidsayuki/tutorial8/MainActivity.java
@@ -0,0 +1,55 @@
+package com.liquidsayuki.tutorial8;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.liquidsayuki.tutorial8.adapter.RecyclerViewAdapter;
+import com.liquidsayuki.tutorial8.databinding.ActivityMainBinding;
+import com.liquidsayuki.tutorial8.model.CourseResult;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView.LayoutManager layoutManager;
+    private List<CourseResult> units;
+    private RecyclerViewAdapter adapter;
+    private ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        units=new ArrayList<CourseResult>();
+        units = CourseResult.createContactsList();
+        adapter = new RecyclerViewAdapter(units);
+        //this just creates a line divider between rows
+        binding.recyclerView.addItemDecoration(new
+                DividerItemDecoration(this,LinearLayoutManager.VERTICAL));
+        binding.recyclerView.setAdapter(adapter);
+        layoutManager = new LinearLayoutManager(this);
+        binding.recyclerView.setLayoutManager(layoutManager);
+        binding.addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String unit = binding.etUnit.getText().toString().trim();
+                String smark= binding.etMark.getText().toString().trim();
+                if (!unit.isEmpty() || !smark.isEmpty()) {
+                    int mark=new Integer(smark).intValue();
+                    saveData(unit, mark);
+                }
+            }
+        });
+    }
+    private void saveData(String unit, int mark) {
+        CourseResult courseResult = new CourseResult(unit, mark);
+        units.add(courseResult);
+        adapter.addUnits(units);
+    }
+}
